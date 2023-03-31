b821f64d513bd809f2afb29b4da02db4c76c3e27
Titan43 <ukrplay73@gmail.com>
2023-03-31 20:09:41 +0300

    implemented itemLoading

--- scripts/buttonControll.js
+++
@@ -1,8 +0,0 @@
-const loginBtn = document.getElementById('login-btn');
-const registerBtn = document.getElementById('register-btn');
-
-const token = document.cookie.split(';').some((c) => c.trim().startsWith('token='));
-if (token) {
-    document.getElementById('login-btn').style.display = 'none';
-    document.getElementById('register-btn').style.display = 'none';
-}
